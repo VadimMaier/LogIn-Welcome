d345d3152f6e91916e2d9ba047f1d6d9b372963c
Вадим <vadim_ponomarev_1989@mail.ru>
2021-02-01 22:45:34 -0300

    Initial Commit

---
+++ LogIn&Welcome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogIn&Welcome
+//
+//  Created by Вадим on 01.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
